ba3fdda9b0002e1c6c85d223e6fc4f397e65a6a2
Moer <199525807@qq.com>
2016-10-11 12:55:38 +0800

    解决sui的问题 本地拷贝下来的样式居然不全，ctm淡

--- dist/js/index.js
+++ dist/js/index.js
@@ -36,5 +36,56 @@ $(function () {
         });
     }
     initBanner();
+
+    // 影视详情页
+    var $detailsMov = $('.details-mov');
+
+    // $detailsMov.find('.tab:first').addClass('active');
+
+    // 简介
+    $detailsMov.on('click', '.button-group .btn', function () {
+        var $this = $(this);
+        // 立即购买
+        if ($this.hasClass('buy')) {
+            // $.toast('操作成功，正在跳转...', 2345);
+            $.msg('账户余额不足，请充值或使用微信支付！');
+        }
+
+        // 加入购物车
+        if ($this.hasClass('cart')) {}
+    });
+
+    // 选集
+    $detailsMov.on('click', '.num', function () {
+        var buttons1 = [{
+            text: '请选择',
+            label: true
+        }, {
+            text: '卖出',
+            bold: true,
+            color: 'danger',
+            onClick: function onClick() {
+                $.alert("你选择了“卖出“");
+            }
+        }, {
+            text: '买入',
+            onClick: function onClick() {
+                $.alert("你选择了“买入“");
+            }
+        }];
+        var buttons2 = [{
+            text: '取消',
+            bg: 'danger'
+        }];
+        var groups = [buttons1, buttons2];
+        $.actions(groups);
+    });
+
+    // 我要报错
+    var $feedback = $('#detail-tab3');
+    var $subText = $feedback.find('.sub-text');
+    $feedback.on('click', '.sub-tag span', function () {
+        $subText.val($subText.val() + '#' + $(this).text() + '#');
+    });
 });
 //# sourceMappingURL=index.js.map
---
+++ src/js/public.js
@@ -0,0 +1,24 @@
+$.extend({
+    msg: function(opts,timeout){
+        let text = opts.text || opts
+        let title = opts.title || '温馨提示'
+        timeout = opts.timeout || 2000
+        
+        let $tpl = $(`
+            <div class="mask">
+                <div class="msg">
+                    <p class="msg-title">${title}</p>
+                    <span class="msg-text">${text}</span>
+                </div>
+            </div>
+        `);
+
+        $('body').append($tpl)
+
+        if (timeout) {
+            setTimeout(function() {
+                $tpl.remove()
+            }, timeout);
+        }
+    }
+})
