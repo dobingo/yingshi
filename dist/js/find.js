b6b9479c290ed204f7d791a2d9dd263a7ca817a7
Moer <199525807@qq.com>
2016-10-13 14:19:31 +0800

    发现页面

--- dist/js/find.js
+++ dist/js/find.js
@@ -18,11 +18,25 @@
             }
         });
 
-        $selectSwitch.picker({
-            toolbarTemplate: '<header class="bar bar-nav">\n            <button class="button button-link pull-left">按钮</button>\n            <button class="button button-link pull-right close-picker">确定</button>\n            <h1 class="title">标题</h1>\n            </header>',
+        $selectSwitch.eq(0).picker({
+            toolbarTemplate: '<header class="find-select"></header>',
             cols: [{
                 textAlign: 'center',
-                values: ['iPhone 4', 'iPhone 4S', 'iPhone 5', 'iPhone 5S', 'iPhone 6', 'iPhone 6 Plus', 'iPad 2', 'iPad Retina', 'iPad Air', 'iPad mini', 'iPad mini 2', 'iPad mini 3']
+                values: ['更新时间', '人气排行']
+            }]
+        });
+        $selectSwitch.eq(1).picker({
+            toolbarTemplate: '<header class="find-select"></header>',
+            cols: [{
+                textAlign: 'center',
+                values: ['全部', '内地', '韩国']
+            }]
+        });
+        $selectSwitch.eq(2).picker({
+            toolbarTemplate: '<header class="find-select"></header>',
+            cols: [{
+                textAlign: 'center',
+                values: ['全部', '电视剧', '电影']
             }]
         });
     })();
--- src/js/find.js
+++ src/js/find.js
@@ -15,15 +15,26 @@
         }
     })
 
-    $selectSwitch.picker({
-        toolbarTemplate: `<header class="bar bar-nav">
-            <button class="button button-link pull-left">按钮</button>
-            <button class="button button-link pull-right close-picker">确定</button>
-            <h1 class="title">标题</h1>
-            </header>`,
+
+    $selectSwitch.eq(0).picker({
+        toolbarTemplate: `<header class="find-select"></header>`,
         cols: [{
             textAlign: 'center',
-            values: ['iPhone 4', 'iPhone 4S', 'iPhone 5', 'iPhone 5S', 'iPhone 6', 'iPhone 6 Plus', 'iPad 2', 'iPad Retina', 'iPad Air', 'iPad mini', 'iPad mini 2', 'iPad mini 3']
+            values: ['更新时间','人气排行']
+        }]
+    });
+    $selectSwitch.eq(1).picker({
+        toolbarTemplate: `<header class="find-select"></header>`,
+        cols: [{
+            textAlign: 'center',
+            values: ['全部','内地','韩国']
+        }]
+    });
+    $selectSwitch.eq(2).picker({
+        toolbarTemplate: `<header class="find-select"></header>`,
+        cols: [{
+            textAlign: 'center',
+            values: ['全部','电视剧','电影']
         }]
     });
 }
