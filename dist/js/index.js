03e35bead28b9cabeaf796ac54bbda56cd10d4a2
Moer <199525807@qq.com>
2016-10-17 16:49:33 +0800

    对接首页接口

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,28 +1,43 @@
-'use strict';
+"use strict";
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+// updateStatus 更新状态 0未完结1完结
+// updateSite 就是更新的集数 数字标识
 
 $(function () {
+    // 开启loading效果
+    $.showPreloader();
 
-    // 搜索展开
-    $('.search-open').click(function () {
-        $('.search-tools').toggleClass('search-show');
+    // 首页
+    $.ajax({
+        type: "get",
+        dataType: "json",
+        url: "http://118.178.136.60:8001/rest/index/index",
+        success: function success(res) {
+            // console.log(res);
+
+            initBanner(res.headerRes);
+            initBest(res.bestMovies);
+            initMain(res.mainRes);
+            initRankTop(res.rankTop);
+        },
+        error: function error(e) {
+            console.log('banner加载出错', e);
+        },
+        complete: function complete() {
+            // 加载完毕，关闭loading效果
+            $.hidePreloader();
+        }
     });
 
-    // ajax
-    /*$.ajax({
-        type: "get",
-        dataType:"json",
-        url: "../json/index.json"
-    })
-    .done((res) => {
-        // 获取数据后，加载每个组件
-    })
-    .fail((e) => {
-        console.log(e);
-    })*/
-
     // 加载banner数据
-    function initBanner() {
-        // 初始化
+    function initBanner(bannerData) {
+        $('#banner img').each(function (i, img) {
+            img.src = bannerData[i].pUrl;
+        });
+
+        // 初始化 swiper组件
         $("#banner").swiper({
 
             // direction: 'vertical',
@@ -34,6 +49,143 @@ $(function () {
             pagination: '.swiper-pagination'
         });
     }
-    initBanner();
+
+    // 加载精品推荐
+    function initBest(movs) {
+        var tpl = "";
+        for (var i = 0; i < movs.length; i++) {
+            var mov = movs[i];
+            tpl += "\n            <li>\n                <a href=\"#page-details-mov\">\n                    <img src=\"" + mov.poster + "\" />\n                    <p class=\"name\">" + (mov.updateStatus == 0 ? '更新中' : '已完结') + "</p>\n                    <p class=\"text\">" + mov.introduction + "</p>\n                </a>\n            </li>\n            ";
+        }
+        $('#rec1 ul').append(tpl);
+    }
+
+    // 加载主打推荐
+    function initMain(res) {
+        $('.recommended-2 a').each(function (i, el) {
+            var $el = $(el);
+            $el.find('img').attr('src', res[i].pictrueUrl);
+            $el.find('.titleInfo').text(res[i].title);
+            $el.find('.content-text').text(res[i].introduction);
+        });
+    }
+
+    // 加载今日热门
+    function initRankTop(res) {
+        var tpl = "";
+        for (var i = 0; i < res.length; i++) {
+            var r = res[i];
+            tpl += "\n            <li>\n                <a class=\"flexlist\" href=\"#page-details-mov\">\n                    <div class=\"imgbox\">\n                        <img src=\"" + r.poster + "\" alt=\"\">\n                    </div>\n                    <div class=\"info\">\n                        <span class=\"t\"><span class=\"index\">01</span>" + r.title + "</span>\n                        <p class=\"text\">" + r.introduction + "</p>\n                        <span class=\"text2\">更新到第" + r.updateSite + "集</span>\n                    </div>\n                </a>\n            </li>\n            ";
+        }
+        $('#rankTop').append(tpl);
+    }
+
+    // 排行榜
+    function initRanking(contaier) {
+        function _loadRank(contaier, data) {
+            var tpl = "";
+            for (var i = 0; i < data.length; i++) {
+                var d = data[0];
+                tpl += "\n                <li>\n                    <a class=\"flexlist\" href=\"#page-details-mov\">\n                        <div class=\"imgbox\">\n                            <img src=\"" + d.poster + "\" alt=\"\">\n                        </div>\n                        <div class=\"info\">\n                            <span class=\"t\"><span class=\"index\">01</span>" + d.title + "</span>\n                            <p class=\"text\">" + d.introduction + "</p>\n                            <span class=\"text2\">更新到第" + d.updateSite + "集</span>\n                        </div>\n                    </a>\n                </li>\n                ";
+            }
+            $(contaier).empty().append(tpl);
+        }
+        // 排行
+        $.showPreloader();
+        $.ajax({
+            type: "get",
+            dataType: "json",
+            url: "http://118.178.136.60:8001/rest/index/ranking",
+            success: function success(res) {
+                // console.log(res);
+
+                if (res.STATUS == 1) {
+                    _loadRank(contaier, res.RANK_LIST);
+                } else {
+                    // 没有数据
+                }
+
+                initRanking(res.headerRes);
+            },
+            error: function error(e) {
+                console.log('排行加载出错', e);
+            },
+            complete: function complete() {
+                $.hidePreloader();
+            }
+        });
+    }
+    var rankTab = $('.ranking .tab-link');
+    var rankContainer = $('.ranking .hotlist');
+    rankTab.one('click', function () {
+        initRanking(rankContainer[$(this).index()]);
+    });
+    $('#rank-indexbtn').one('click', function () {
+        // 点击排行tab首次加载 今日热门
+        initRanking(rankContainer[0]);
+    });
+
+    // 搜索功能
+    var searchInputs = $('.search-tools input');
+    searchInputs.on('input', function () {
+        // 首页和搜索页的输入框双向绑定
+        searchInputs.not($(this)).val($(this).val());
+    });
+    $(document).on('click', '.search', function (e) {
+        e.stopPropagation();
+        var $this = $(this);
+        // 搜索框的展开和收起
+        if ($this.hasClass('search-open')) {
+            $('.search-tools').toggleClass('search-show');
+        }
+
+        // 进行搜索
+        if ($this.hasClass('search-btn')) {
+            var _ret = function () {
+                var searchName = $this.parent().find('input').val();
+                if (!searchName) {
+                    return {
+                        v: void 0
+                    };
+                }
+                var $ul = $('.search-list ul');
+                $.showPreloader();
+                $.ajax({
+                    type: "get",
+                    url: "http://118.178.136.60:8001/rest/index/search",
+                    data: {
+                        searchName: searchName
+                    },
+                    dataType: 'json',
+                    success: function success(res) {
+                        // console.log(res);
+                        var listTpl = "";
+                        $ul.empty(); //先清空list
+                        if (res.STATUS == 1) {
+                            var movs = res.MOVIES.content;
+                            for (var i = 0; i < movs.length; i++) {
+                                var mov = res.MOVIES.content[i];
+                                var index = mov.id < 10 ? '0' + mov.id : mov.id;
+                                listTpl += "\n                            <li>\n                                <a class=\"flexlist\" href=\"#\">\n                                    <div class=\"imgbox\">\n                                        <img src=\"" + mov.poster + "\" />\n                                    </div>\n                                    <div class=\"info\">\n                                        <span class=\"t\"><span class=\"index\">" + index + "</span>" + mov.title + "</span>\n                                        <p class=\"text\">" + mov.introduction + "</p>\n                                        <span class=\"text2\">更新到第" + mov.updateSite + "集</span>\n                                    </div>\n                                </a>\n                            </li>\n                           ";
+                            }
+                            $ul.append(listTpl);
+                            $ul.show();
+                        } else {
+                            $ul.hide();
+                        }
+                    },
+                    error: function error(e) {
+                        console.log(e);
+                        $.alert('搜索出错，请稍后再试');
+                    },
+                    complete: function complete() {
+                        $.hidePreloader();
+                    }
+                });
+            }();
+
+            if ((typeof _ret === "undefined" ? "undefined" : _typeof(_ret)) === "object") return _ret.v;
+        }
+    });
 });
 //# sourceMappingURL=index.js.map
--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,40 @@
+// updateStatus 更新状态 0未完结1完结
+// updateSite 就是更新的集数 数字标识
+
 $(function () {
+    // 开启loading效果
+    $.showPreloader();
 
-    // 搜索展开
-    $('.search-open').click(function () {
-        $('.search-tools').toggleClass('search-show');
-    })
-
-    // ajax
-    /*$.ajax({
+    // 首页
+    $.ajax({
         type: "get",
-        dataType:"json",
-        url: "../json/index.json"
-    })
-    .done((res) => {
-        // 获取数据后，加载每个组件
-    })
-    .fail((e) => {
-        console.log(e);
-    })*/
+        dataType: "json",
+        url: "http://118.178.136.60:8001/rest/index/index",
+        success: (res) => {
+            // console.log(res);
 
+            initBanner(res.headerRes)
+            initBest(res.bestMovies)
+            initMain(res.mainRes)
+            initRankTop(res.rankTop)
+
+        },
+        error: (e) => {
+            console.log('banner加载出错', e);
+        },
+        complete: () => {
+            // 加载完毕，关闭loading效果
+            $.hidePreloader();
+        }
+    })
 
     // 加载banner数据
-    function initBanner() {
-        // 初始化
+    function initBanner(bannerData) {
+        $('#banner img').each((i, img) => {
+            img.src = bannerData[i].pUrl
+        })
+
+        // 初始化 swiper组件
         $("#banner").swiper({
 
             // direction: 'vertical',
@@ -33,8 +46,190 @@ $(function () {
             pagination: '.swiper-pagination'
         });
     }
-    initBanner()
 
+    // 加载精品推荐
+    function initBest(movs) {
+        let tpl = ``;
+        for (let i = 0; i < movs.length; i++) {
+            let mov = movs[i]
+            tpl += `
+            <li>
+                <a href="#page-details-mov">
+                    <img src="${mov.poster}" />
+                    <p class="name">${mov.updateStatus == 0 ? '更新中' : '已完结'}</p>
+                    <p class="text">${mov.introduction}</p>
+                </a>
+            </li>
+            `
+        }
+        $('#rec1 ul').append(tpl)
+    }
+
+    // 加载主打推荐
+    function initMain(res) {
+        $('.recommended-2 a').each((i, el) => {
+            let $el = $(el)
+            $el.find('img').attr('src', res[i].pictrueUrl)
+            $el.find('.titleInfo').text(res[i].title)
+            $el.find('.content-text').text(res[i].introduction)
+        })
+    }
+
+    // 加载今日热门
+    function initRankTop(res) {
+        let tpl = ``
+        for (let i = 0; i < res.length; i++) {
+            let r = res[i]
+            tpl += `
+            <li>
+                <a class="flexlist" href="#page-details-mov">
+                    <div class="imgbox">
+                        <img src="${r.poster}" alt="">
+                    </div>
+                    <div class="info">
+                        <span class="t"><span class="index">01</span>${r.title}</span>
+                        <p class="text">${r.introduction}</p>
+                        <span class="text2">更新到第${r.updateSite}集</span>
+                    </div>
+                </a>
+            </li>
+            `
+        }
+        $('#rankTop').append(tpl)
+    }
+
+
+
+    // 排行榜
+    function initRanking(contaier) {
+        function _loadRank(contaier, data) {
+            let tpl = ``
+            for (let i = 0; i < data.length; i++) {
+                let d = data[0]
+                tpl += `
+                <li>
+                    <a class="flexlist" href="#page-details-mov">
+                        <div class="imgbox">
+                            <img src="${d.poster}" alt="">
+                        </div>
+                        <div class="info">
+                            <span class="t"><span class="index">01</span>${d.title}</span>
+                            <p class="text">${d.introduction}</p>
+                            <span class="text2">更新到第${d.updateSite}集</span>
+                        </div>
+                    </a>
+                </li>
+                `
+            }
+            $(contaier).empty().append(tpl)
+        }
+        // 排行
+        $.showPreloader();
+        $.ajax({
+            type: "get",
+            dataType: "json",
+            url: "http://118.178.136.60:8001/rest/index/ranking",
+            success: (res) => {
+                // console.log(res);
+
+                if (res.STATUS == 1) {
+                    _loadRank(contaier, res.RANK_LIST)
+                } else {
+                    // 没有数据
+                }
+
+                initRanking(res.headerRes)
+
+            },
+            error: (e) => {
+                console.log('排行加载出错', e);
+            },
+            complete: () => {
+                $.hidePreloader();
+            }
+        })
+    }
+    let rankTab = $('.ranking .tab-link')
+    let rankContainer = $('.ranking .hotlist')
+    rankTab.one('click',function () {
+        initRanking(rankContainer[$(this).index()])
+    })
+    $('#rank-indexbtn').one('click', function () {
+        // 点击排行tab首次加载 今日热门
+        initRanking(rankContainer[0])
+    })
+
+
+
+    // 搜索功能
+    let searchInputs = $('.search-tools input')
+    searchInputs.on('input', function () {
+        // 首页和搜索页的输入框双向绑定
+        searchInputs.not($(this)).val($(this).val())
+    })
+    $(document).on('click', '.search', function (e) {
+        e.stopPropagation();
+        let $this = $(this)
+            // 搜索框的展开和收起
+        if ($this.hasClass('search-open')) {
+            $('.search-tools').toggleClass('search-show');
+        }
+
+        // 进行搜索
+        if ($this.hasClass('search-btn')) {
+            let searchName = $this.parent().find('input').val();
+            if (!searchName) {
+                return;
+            }
+            let $ul = $('.search-list ul');
+            $.showPreloader();
+            $.ajax({
+                type: "get",
+                url: "http://118.178.136.60:8001/rest/index/search",
+                data: {
+                    searchName: searchName
+                },
+                dataType:'json',
+                success: function (res) {
+                    // console.log(res);
+                    let listTpl = ``;
+                    $ul.empty(); //先清空list
+                    if (res.STATUS == 1) {
+                        let movs = res.MOVIES.content
+                        for (let i = 0; i < movs.length; i++) {
+                            let mov = res.MOVIES.content[i]
+                            let index = mov.id < 10 ? '0' + mov.id : mov.id
+                            listTpl += `
+                            <li>
+                                <a class="flexlist" href="#">
+                                    <div class="imgbox">
+                                        <img src="${mov.poster}" />
+                                    </div>
+                                    <div class="info">
+                                        <span class="t"><span class="index">${index}</span>${mov.title}</span>
+                                        <p class="text">${mov.introduction}</p>
+                                        <span class="text2">更新到第${mov.updateSite}集</span>
+                                    </div>
+                                </a>
+                            </li>
+                           `
+                        }
+                        $ul.append(listTpl)
+                        $ul.show();
+                    } else {
+                        $ul.hide();
+                    }
+                },
+                error: function (e) {
+                    console.log(e);
+                    $.alert('搜索出错，请稍后再试')
+                },
+                complete: () => {
+                    $.hidePreloader();
+                }
+            });
+        }
+    })
 
 
 
